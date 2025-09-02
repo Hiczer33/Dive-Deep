34b324c5a42bdc8c331f0aac331bd5e126a38fac
Jesper <jesperlundthingholm@hotmail.com>
2025-09-02 12:37:34 +0200

    Added model for BCD and Enumtypes

---
+++ diveDeep/diveDeep/Persistence/BCDRepository.cs
@@ -0,0 +1,22 @@
+﻿using diveDeep.Models;
+using diveDeep.Enum;
+
+
+namespace diveDeep.Persistence
+{
+    public static class BCDRepository
+    {
+        private static readonly List<BCD> _bcd = new()
+        {
+            new BCD
+            {
+                Brand = "Scubapro",
+                Model = "Navigator Lite BCD",
+                Size = new List<SizeType> { SizeType.S, SizeType.M, SizeType.L },
+                PricePrDay = 125
+            }
+
+
+        };
+    }
+}
