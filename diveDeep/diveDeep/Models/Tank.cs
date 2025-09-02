5afbda892c58b508f96e0dc660215c3ca4721237
Dennis Jensen <deje67370@edu.ucl.dk>
2025-09-02 14:04:16 +0200

    added tankRepository

---
+++ diveDeep/diveDeep/Models/Tank.cs
@@ -0,0 +1,10 @@
+﻿namespace diveDeep.Models
+{
+    public class Tank
+    {
+        public string Brand { get; set; }
+        public double Volume { get; set; }
+
+        public double PricePrDay { get; set; }
+    }
+}
