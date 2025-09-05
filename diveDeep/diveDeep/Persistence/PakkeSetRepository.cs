f8598c563a1006d641ad1903f95b35437f6835f2
Jesper <jesperlundthingholm@hotmail.com>
2025-09-05 12:32:37 +0200

    Created PakkeDykkerSæt

---
+++ diveDeep/diveDeep/Persistence/PakkeSetRepository.cs
@@ -0,0 +1,24 @@
+﻿using diveDeep.Models;
+
+namespace diveDeep.Persistence
+{
+    public static class PakkeSetRepository
+    {
+
+        private static readonly List<PakkeSet> _pakkeset = new()
+        {
+
+            new PakkeSet
+            {
+                Title = "Komplet Dykkersæt",
+                PricePrDay = 500,
+                SetInformation = "BCD, Dykkerdragt, Regulatorsæt, Tank, Finner, Maske, Snorkel",
+                ImageUrl = "\\Images\\DykkerSet\\kompletdykkerset.webp"
+            }
+
+           
+        };
+        public static IEnumerable<PakkeSet> GetAll() => _pakkeset;
+
+    }
+}
