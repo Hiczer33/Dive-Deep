e803a1f12962927642061c892dcdf72ab0562870
Jesper <jesperlundthingholm@hotmail.com>
2025-09-09 12:18:57 +0200

    Added description to our repo

--- diveDeep/diveDeep/Persistence/BCDRepository.cs
+++ diveDeep/diveDeep/Persistence/BCDRepository.cs
@@ -14,7 +14,8 @@ namespace diveDeep.Persistence
                 Model = "Navigator Lite BCD",
                 Size = new List<SizeType> { SizeType.S, SizeType.M, SizeType.L },
                 PricePrDay = 125,
-                ImageUrl = "\\Images\\BCD_Pictures\\Scubapro_Navigator Lite BCD.webp"
+                ImageUrl = "\\Images\\BCD_Pictures\\Scubapro_Navigator Lite BCD.webp",
+                Description = "Scubapro Navigator Lite er en ny, letvægts ryginflations-BCD, designet specielt til den rekreative rejse-dykker. Den kombinerer en enkel, men slidstærk konstruktion med et unikt, modulært design og muligheden for at personliggøre udstyret med forskellige farvekombinationer. Med sin foldbare Airnet®-rygplade, robust materialevalg og udskiftelige komponenter er Navigator Lite ideel til rejser og dykning under mange forskellige forhold"
             },
 
             new BCD
@@ -23,7 +24,9 @@ namespace diveDeep.Persistence
                 Model = "BCD Glide",
                 Size = new List<SizeType> { SizeType.S, SizeType.M, SizeType.L },
                 PricePrDay = 140,
-                ImageUrl ="\\Images\\BCD_Pictures\\scubapro_bcdglide.webp"
+                ImageUrl ="\\Images\\BCD_Pictures\\scubapro_bcdglide.webp",
+                Description = "Glide BCD'en fra Scubapro er en front-justerbar BCD i traditionel stil, primært rettet til det mandlige køn. Den kvindelige model findes også her på siden under navnet Bella.\r\n\r\nBåde Glide og Bella er designet til at yde maksimal komfort og performance, hvilket tydeligt ses i den velpolstrede og bløde inderside, samt det robuste ydre med mange d-ringe, lommer og Scubapro's power-inflator. Begge BCD'er kommer med y-formede skulderstropper der sidder optimalt over bryst og skulder, og samtidig øger rækkevide og bevægelighed i skulderleddet. Bella BCD'en er her optimeret til kvinder med større brystparti. \r\n\r\nPå BCD'en er monteret 3 velfungerende dump-ventiler, der tilladder dykkeren at dumpe luften, uanset hvilken position denne måtte have i vandet. Desuden kommer denne type BCD med integrede blylommer, der snildt kan rumme op mod 4 kg i hver side, samt to trimlommer på ryggen der hver kan holde 1 kg."
+
             },
             new BCD
             {
