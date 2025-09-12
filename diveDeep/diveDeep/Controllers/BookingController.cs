8f5ea330b62c8c9e354c8111bf375b048f83fb8a
Alexander Hansen <marvelhimself@gmail.com>
2025-09-12 11:36:14 +0200

    Har prøvet at lave Booking. den skal nok arbejdes på, i forhold til Framework

---
+++ diveDeep/diveDeep/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using diveDeep.Models;
+
+namespace diveDeep.Controllers
+{
+    public class BookingController : Controller
+    {
+ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmBooking(Booking booking)
+        {
+            if (!ModelState.IsValid) return View("Index", booking);
+
+            return RedirectToAction("Success");
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ diveDeep/diveDeep/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace diveDeep.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int PeopleCount { get; set; }
+        public decimal PricePrDay { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                int days = (EndDate - StartDate).Days;
+                if (days < 1) days = 1;
+
+                decimal productSum = 0;
+                foreach (var p in Products)
+                {
+                    productSum += p.PricePrDay;
+                }
+
+                return days * productSum * (PeopleCount > 0 ? PeopleCount : 1);
+            }
+        }
+    }
+}
+
