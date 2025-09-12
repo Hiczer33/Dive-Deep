99bf6df509b06afd13b604edd19217e238848a39
Jesper <jesperlundthingholm@hotmail.com>
2025-09-12 10:09:21 +0200

    Big test

---
+++ diveDeep/diveDeep/Controllers/ListController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using diveDeep.Services;
+
+namespace diveDeep.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ids = ProductListService.GetAll();
+            return View(ids);
+        }
+
+        [HttpPost]
+        public IActionResult Add(int id)
+        {
+            ProductListService.Add(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Clear()
+        {
+            ProductListService.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ diveDeep/diveDeep/Services/ProductListService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace diveDeep.Services
+{
+    public static class ProductListService
+    {
+        private static readonly List<int> _ids = new();
+
+        public static IReadOnlyList<int> GetAll() => _ids;
+
+        public static void Add(int productId) => _ids.Add(productId);
+
+        public static void Clear() => _ids.Clear();
+    }
+}
